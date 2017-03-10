6a23d99496c51b60a3066eb8573b630656a5a763
JoshWinebrener <joshua.winebrener@gmail.com>
2017-03-10 08:40:41 -0600

    Added comments throughout
    Because vulcan mind melds don't work as well as I thought

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var yPos = 0;
 var lastYPos = 0;
 var x = 0;
+//change this value if you don't want the screen to be as colorful
 let hueCap = 200;
 
+//draws a red line followed by a multicolor one in front of the points
 var leadingLine = function(xPos, highRed, highGreen, highBlue, lowRed, lowGreen, lowBlue) {
 	
 	stroke(255, 0, 0);
@@ -15,21 +17,18 @@ var leadingLine = function(xPos, highRed, highGreen, highBlue, lowRed, lowGreen,
 	
 }
 
+//draws a point that is +- 10 pixels away from the previous one
 var randomPoint = function(xPos) {
 
-	if (lastYPos < (height - 10) && lastYPos > 10) {
-
+	//draw a point +- 10 pixels higher thant he last one if it is well within the boundaries of the screen
+	if (lastYPos < (height - 10) && lastYPos > 10)
 		yPos = random((lastYPos + 10), (lastYPos - 10));
-
-	} else if (lastYPos > (height - 10)) {
-
+	//bounce down from the top of the screen
+	else if (lastYPos > (height - 10))
 		yPos = random(height - 40, height);
-
-	} else if (lastYPos < 10) {
-
+	//bounce down from the bottom of the screen
+	else if (lastYPos < 10)
 		yPos = random(0, 40);
-
-	}
 	
 	line(xPos - 1, lastYPos, xPos, yPos);
 
@@ -37,14 +36,17 @@ var randomPoint = function(xPos) {
 }
 
 function setup () {
+	//set up canvas
 	createCanvas(windowWidth, windowHeight);
 	background(255);
 	stroke(0);
 	strokeWeight(3);
 	
+	//create a random starting y position
 	yPos = random(height);
 	lastYPos = yPos;
 	
+	//randomize colors of multicolor line
 	highRed = random(255);
 	highGreen = random(255);
 	highBlue = random(255);
@@ -58,11 +60,14 @@ function draw() {
 	leadingLine(x, highRed, highGreen, highBlue, lowRed, lowGreen, lowBlue);
 	randomPoint(x);
 	
+	//increment x to move the bar and needle along
 	x++;
+	//start over if the bar goes past the screen
 	if (x >= width) {
 		x = 0;
 	}
 	
+	//randomly change each color of the multicolor line to +- 10 of the last one
 	highRed = random(highRed - 10, highRed + 10);
 	highGreen = random(highGreen - 10, highGreen + 10);
 	highBlue = random(highBlue - 10, highBlue + 10);
@@ -101,4 +106,4 @@ function draw() {
 	} else if (lowBlue < 0) {
 		lowBlue = random(0, 25);
 	}
-}
+} //draw
